510707ecf302aed217bf480048a621d16c22a233
ashaffir <alfredshaffir@alfreds-MacBook.Home>
2017-01-04 17:53:57 +0200

    Working version

--- realtime_plotting.py
+++ realtime_plotting.py
@@ -1,17 +1,19 @@
+# From: http://stackoverflow.com/questions/11874767/real-time-plotting-in-while-loop-with-matplotlib
 # Realtime plotting
 
 import matplotlib.pyplot as plt
 import numpy as np
 plt.ion() ## Note this correction
 fig=plt.figure()
-plt.axis([0,1000,0,1])
+#  range    X       Y
+plt.axis([0,100,0,50])
 
 i=0
 x=list()
 y=list()
 
 while i <100:
-    temp_y=np.random.random();
+    temp_y=np.random.randint(0,100);
     x.append(i);
     y.append(temp_y);
     plt.scatter(i,temp_y);
